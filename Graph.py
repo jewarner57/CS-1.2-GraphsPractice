c537037bb998339fed9fb1535cd351a1fb2d39b5
Jonathan Warner <jewarner57@gmail.com>
2020-12-08 14:40:12 -0500

    complete numPreReqs method

--- Graph.py
+++ Graph.py
@@ -23,3 +23,29 @@ class Graph:
             for prereq in MSclass["prerequisites"]:
                 self.nodes.get(MSclass.get("name")).add_edge(
                     self.nodes.get(prereq))
+
+    def numPreReqs(self, courseName):
+        # get the course
+        course = self.nodes.get(courseName)
+        prereqs = []
+
+        # check if the coursename is valid
+        if course is None:
+            return "Invalid Course Name"
+
+        # check if course has any prereqs
+        if len(course.edges) == 0:
+            return None
+
+        # get prereqs
+        for prereq in course.edges:
+
+            prereqs.append(prereq)
+
+            nestedPrereq = self.numPreReqs(prereq)
+            if nestedPrereq is not None:
+                for course in nestedPrereq:
+                    prereqs.append(course)
+
+        prereqs = list(dict.fromkeys(prereqs))
+        return prereqs
--- main.py
+++ main.py
@@ -5,9 +5,11 @@ graph.jsonToGraph('./classlist.json')
 
 print(graph.nodes)
 
-for id in graph.nodes:
-    print(id + ":")
-    edges = graph.nodes[id].get_edges()
-    for edge in edges:
-        print("  " + edges[edge].id)
-    print("----------------")
+print(graph.numPreReqs("FEW 2.3"))
+
+# for id in graph.nodes:
+#     print(id + ":")
+#     edges = graph.nodes[id].get_edges()
+#     for edge in edges:
+#         print("  " + edges[edge].id)
+#     print("----------------")
